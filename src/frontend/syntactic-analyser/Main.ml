9a909d035d24d5e813b402d2d6de6fb52db1f7f9
Nikolaos Galanopoulos <el18024@mail.ntua.gr>
2022-07-14 19:04:41 +0300

    Changes to Main.ml
    Now the compiler reads directly from a file and not via output
    redirection. I've added some basic checking (file existence etc.).
    Main.ml now interacts with the parser and lexer via the simpler
    interface that does not use the stack we introduced previously.

--- src/frontend/syntactic-analyser/Main.ml
+++ src/frontend/syntactic-analyser/Main.ml
@@ -1,11 +1,18 @@
 let main =
-  Lexer.init; 
-  try
-    Parser.program Lexer.lexer (Lexer.current_buffer);
-    Printf.eprintf "Okay I think\n";
-    exit 0
-  with _ ->
-    let pos = (Lexer.current_buffer).Lexing.lex_curr_p 
-    in Printf.eprintf "(File '%s' - Line %d) Syntax Error\n" 
-                        pos.pos_fname pos.pos_lnum ;
-    exit 1
+  if (Array.length Sys.argv <> 2) then (Printf.eprintf "Usage: ./edsger <filename>.eds\n")
+  else 
+  (
+    let fn = Sys.argv.(1) in
+      if (not @@ Sys.file_exists fn) then (Printf.eprintf "File: '%s' does not exist\n" fn; exit 1);
+    let c = Stdlib.open_in fn in
+    let lb = Lexing.from_channel c in 
+      Lexing.set_filename lb fn;
+    try
+        Parser.program Lexer.lexer lb; exit 0
+    with _ -> 
+        let pos = lb.Lexing.lex_curr_p in
+        let line_pos = (pos.pos_cnum - pos.pos_bol + 1) in
+        Printf.eprintf "(File: '%s' - Line %d, Column %d) Syntax Error\n" 
+                            pos.pos_fname pos.pos_lnum line_pos
+  );   
+  exit 1;
