bc92b8b2c06333b3c349efab791cfc6b4c9f76e0
Nikolaos Galanopoulos <el18024@mail.ntua.gr>
2022-08-10 12:25:56 +0300

    Added the runtime lib once again.

---
+++ misc/test-programs/runtime-lib/math.h
@@ -0,0 +1,16 @@
+/*    math.h for Edsger       */
+/* Math Function Declarations */
+
+/*      Absolute Values       */
+int abs (int n);
+double fabs (double d);
+
+/*      Approximators :)      */
+double sqrt (double d);   /*     Square Root     */
+double sin (double d);    /*         Sine        */
+double cos (double d);    /*        Cosine       */
+double tan (double d);    /*        Tangent      */ 
+double atan (double d);   /*       Arctangent    */  
+double exp (double d);    /* Natural Exponential */
+double ln (double d);     /*  Natural Logarithm  */  
+double pi ();             /*      Pi constant    */
---
+++ misc/test-programs/runtime-lib/string.h
@@ -0,0 +1,6 @@
+/*     string.h for Edsger      */
+/* String Function Declarations */
+int strlen (char * s);
+int strcmp (char * s1, char * s2);
+void strcpy (char * trg, char * src);
+void strcat (char * trg, char * src);
