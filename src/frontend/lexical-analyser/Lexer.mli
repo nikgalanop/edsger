0471ad6cfb3100937b7c94eb6a67deb9c30a1241
Nikolaos Galanopoulos <el18024@mail.ntua.gr>
2022-08-19 18:27:04 +0300

    Added some documentation with comments in the .mli files.

--- src/frontend/lexical-analyser/Lexer.mli
+++ src/frontend/lexical-analyser/Lexer.mli
@@ -4,4 +4,9 @@ val lexer : Lexing.lexbuf -> Parser.token
 (* Create a buffer from an existing file *)
 val add_file : string -> Lexing.lexbuf 
 
+(* When the lexical analyser fails to do something, like 
+   opening a file etc. it raises a LexFailure exception 
+   that contains metadata about the position that the 
+   error was found, as well as a message describing the
+   error.*)
 exception LexFailure of Lexing.position * string 
