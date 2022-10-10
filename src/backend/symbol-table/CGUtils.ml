46bda17181c77e2da1563fa12dffbf4da3620dad
Nikolaos Galanopoulos <el18024@mail.ntua.gr>
2022-10-10 14:31:24 +0300

    Solving #55

--- src/backend/symbol-table/CGUtils.ml
+++ src/backend/symbol-table/CGUtils.ml
@@ -4,8 +4,7 @@ let get_ast_param_name = function
   | Ast.BYVAL (_, vn) | Ast.BYREF (_, vn) -> vn
 
 let funStr_mangled fnstr pstr num = 
-  let num' = string_of_int num in 
-  let name = fnstr ^ "_" ^ pstr ^ "_" ^ num' in 
+  let name = Printf.sprintf "%s_%s_%d" fnstr pstr num in 
   let cond1 = CGSymbol.inOuterScope () in
   let cond2 = fnstr = "main" in 
   let cond3 = pstr = "" in
