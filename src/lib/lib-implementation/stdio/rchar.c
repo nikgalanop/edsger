f06a960b19e51bd3c24e817e811c51abf7b2002b
Nikolas Galanopoulos <94012652+nikgalanop@users.noreply.github.com>
2023-02-10 22:35:33 +0200

    Fixed `readChar` implementation

--- src/lib/lib-implementation/stdio/rchar.c
+++ src/lib/lib-implementation/stdio/rchar.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
 extern char readChar__0 (){
-	char tmp;
-	scanf(" %c", &tmp); // The space before %c "eats"
-	return tmp; 		// whitespaces and special characters
+	return getchar();
 };
